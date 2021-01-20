4640561023425d44876cb3fa7f8a67fc6ea27afd
burningfiremoon <charlesschool04@gmail.com>
2021-01-20 11:37:30 -0800

    player code done TODO's added

--- main file.py
+++ main file.py
@@ -1,6 +1,7 @@
 import pygame
 
 # ----- CONSTANTS
+BLACK = (0,0,0)
 WIDTH = 800
 HEIGHT = 600
 TITLE = "test subject"
@@ -15,7 +16,97 @@ class Player(pygame.sprite.Sprite):
         height = 60
         self.image = pygame.Surface([width, height])
         #___Hitbox___
-        self.rect = self.image.get_rect().inflate(-30)
+        self.rect = self.image.get_rect()
+
+        #___Speed of the player___
+        self.vel_x = 0
+        self.vel_y = 0
+
+        # Added this cause plagiarism :D
+        self.level = None
+
+    def update(self):
+        """Player's movement"""
+        self.calc_grav()
+
+
+        #___Hit Reg___
+        #_Horizontal_
+        self.rect.x += self.vel_x
+
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            # If we are moving right,
+            # set our right side to the left side of the item we hit
+            if self.vel_x > 0:
+                self.rect.right = block.rect.left
+            elif self.vel_x < 0:
+                # Otherwise if we are moving left, do the opposite.
+                self.rect.left = block.rect.right
+
+        # _Vertical_
+        self.rect.y += self.vel_y
+
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+
+            # Reset our position based on the top/bottom of the object.
+            if self.vel_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.vel_y < 0:
+                self.rect.top = block.rect.bottom
+
+            # Stop our vertical movement
+            self.vel_y = 0
+    def calc_grav(self):
+        """The Earth is flat things just fall"""
+        if self.vel_y == 0:
+            self.vel_y = 1
+        else:
+            self.vel_y += .35
+
+        # See if we are on the ground.
+        if self.rect.y >= HEIGHT - self.rect.height and self.vel_y >= 0:
+            self.vel_y = 0
+            self.rect.y = HEIGHT - self.rect.height
+
+    def jump(self):
+        """ Called when user hits 'jump' button. """
+
+        # move down a bit and see if there is a platform below us.
+        # Move down 2 pixels because it doesn't work well if we only move down
+        # 1 when working with a platform moving down.
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        self.rect.y -= 2
+
+        # If it is ok to jump, set our speed upwards
+        if len(platform_hit_list) > 0 or self.rect.bottom >= HEIGHT:
+            self.vel_y = -10
+
+    # Player-controlled movement:
+    def go_left(self):
+        """ Called when the user hits the left arrow. """
+        self.vel_x = -6
+
+    def go_right(self):
+        """ Called when the user hits the right arrow. """
+        self.vel_x = 6
+
+    def stop(self):
+        """ Called when the user lets off the keyboard. """
+        self.vel_x = 0
+
+# TODO: platforms
+# TODO: walls
+# TODO: player sprites
+# TODO: bullet sprites
+# TODO: sounds
+# TODO: stages
+# TODO: animation
+
+
+
 
 
 def main():
